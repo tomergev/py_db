4326938f7621dbda0b7f734e5268fe8bb2f15555
tomergev <tomer@whiteblock.io>
2018-08-21 16:51:41 -0700

    Worked more on the routing with flask

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-	return "First route everrr"
+# Stackover flow discussing flask routes: https://stackoverflow.com/questions/10434599/how-to-get-data-received-in-flask-request
+@app.route("/users", methods=["POST"])
+def parse_request():
+	print request.form
+	print request.get_json(force=True)
+	return "Hello World"
 
 if __name__ == "__main__":
 	app.run(debug=True)
